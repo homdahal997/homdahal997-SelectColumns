60e192bd3b583fb4056db3171951481caf3f9f8f
homdahal997 <homdahal997@gmail.com>
2023-03-07 15:12:21 +0000

    selecting only column from table

---
+++ problem1.sql
@@ -0,0 +1,14 @@
+/**
+     * problem 1: Write the SQL statement in the problem1.sql file to return only the 'firstname' column from the
+     * site_user table
+     *
+     *      site_user table:
+     *      |   id  |     firstname        |        lastname        |
+     *      ----------------------------------------------------------
+     *      |1      |'Steve'               |'Garcia'                |
+     *      |2      |'Alexa'               |'Smith'                 |
+     *      |3      |'Steve'               |'Jones'                 |
+     *      |4      |'Brandon'             |'Smith'                 |
+     *      |5      |'Adam'                |'Jones'                 |
+     */
+select firstname from site_user;
